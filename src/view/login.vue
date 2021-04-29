fab457d4c3844a83ce99b466dddaf9af4c09e636
xrx <xrx@xrx.cn>
2021-04-29 10:42:28 +0800

    登录界面制作完成

--- src/view/login.vue
+++ src/view/login.vue
@@ -1,14 +1,96 @@
 <template>
-  <div class="login"></div>
+  <div class="login">
+    <div class="container">
+      <img src="@/assets/avatar.jpg" alt="" class="avatar" />
+      <el-form
+        :model="loginForm"
+        :rules="rules"
+        ref="ruleForm"
+        class="demo-ruleForm"
+      >
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="el-icon-user-solid"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="el-icon-key"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" round class="login-btn">主要按钮</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [
+          // required: true:必填项   message：不符合规则时的提示信息  trigger:触发时机
+          { required: true, message: "请输入11位手机号码", trigger: "blur" },
+          // pattern：正则规则
+          {
+            pattern: /^1[35789]\d{9}$/,
+            message: "请输入11位手机号码,首位为1",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          // required: true:必填项   message：不符合规则时的提示信息  trigger:触发时机
+          { required: true, message: "请输入密码", trigger: "blur" },
+          // pattern：正则规则
+          {
+            pattern: /^.{3,16}$/,
+            message: "请输入3到16位密码",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
   },
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #2f4050;
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    background: white;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
 </style>
